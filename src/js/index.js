3d922533f8b0d4bce99cdbb6a9fceab9886d4342
Traniks <andert2017@gmail.com>
2023-04-30 16:35:52 +0300

    2 slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,40 @@
-$(document).ready(function(){
-    $('.slider__inner').slick({
-        infinite: true,
-        slidesToShow: 1,
-        speed: 600,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arrow_right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
-  });
+// slick-slider
+// $(document).ready(function(){
+//     $('.slider__inner').slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         speed: 600,
+//         adaptiveHeight: true,
+//         autoplay: true,
+//         autoplaySpeed: 5000,
+//         prevArrow: '<button type="button" class="prev"><img src="icons/slider/arrow_left.png"></button>',
+//         nextArrow: '<button type="button" class="next"><img src="icons/slider/arrow_right.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+//   });
+
+// tiny-slider
+// const slider = tns({
+//     container: '.slider__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     autoplayTimeout: 1000,
+//     controls: false,
+//     nav: true,
+//     navPosition: "bottom"
+// });
+// document.querySelector(".prev").addEventListener("click", function() {
+//     slider.goTo("prev");
+// });
+// document.querySelector(".next").addEventListener("click", function() {
+//     slider.goTo("next");
+// });
