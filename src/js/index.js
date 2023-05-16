085535dfe1bdb14e2ff51739f2dfe225ae8f8361
Traniks <andert2017@gmail.com>
2023-05-16 16:53:02 +0300

    1/2 validator + phone mask

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,4 @@
-// slick-slider
-// $(document).ready(function(){
-//     $('.slider__inner').slick({
-//         infinite: true,
-//         slidesToShow: 1,
-//         speed: 600,
-//         adaptiveHeight: true,
-//         autoplay: true,
-//         autoplaySpeed: 5000,
-//         prevArrow: '<button type="button" class="prev"><img src="icons/slider/arrow_left.png"></button>',
-//         nextArrow: '<button type="button" class="next"><img src="icons/slider/arrow_right.png"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-//             }
-//         ]
-//     });
-//   });
-
-// tiny-slider
+// tiny-slider start
 document.addEventListener("DOMContentLoaded", () => {
     const slider = tns({
         container: '.slider__inner',
@@ -41,7 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
         slider.goTo("next");
     });
     // tiny-slider end
-    // catalog
+
+    // Catalog start
     $('ul.catalog__menu').on('click', 'li:not(.active)', function() {
         $(this)
             .addClass('catalog__menu-item_active').siblings().removeClass('catalog__menu-item_active')
@@ -60,25 +38,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     toggleClass('.catalog-item__link');
     toggleClass('.catalog-item__back');
-    
-    // catalog end
+    // Catalog end
 
-    // const map = document.querySelector("#mapDiv > div > div > div:nth-child(5) > div");
-    
-        
-    // setInterval(()=> {
-    //     map.style.display = "none";
-    //     console.log(map);
-    // }, 1000);
-
-    // Modal
-
+    // Modal start
     const cross = document.querySelectorAll(".modal__close");
     const promo_buttons = document.querySelectorAll("[data-modal=consultation]");
-    const promo_button = document.querySelector("[data-modal=consultation]");
+    // const promo_button = document.querySelector("[data-modal=consultation]");
     const catalog_buttons = document.querySelectorAll(".button_catalog"); 
-    const overlay = document.querySelector(".overlay");
-    const buttons = document.querySelectorAll(".button")
+    // const overlay = document.querySelector(".overlay");
+    // const buttons = document.querySelectorAll(".button")
 
     promo_buttons.forEach(elem => {
        elem.addEventListener("click", () => {
@@ -98,8 +66,8 @@ document.addEventListener("DOMContentLoaded", () => {
         elem.addEventListener("click", () => {
             fadeOut(".overlay");
             fadeOut("#consultation");
-            fadeOut("#thanks");
             fadeOut("#order");
+            fadeOut("#thanks");
         });
     });
 
@@ -126,5 +94,60 @@ document.addEventListener("DOMContentLoaded", () => {
             opacity += opacity * 0.1;
         }, 10);
     }
+    // Modal end
+
+    // Validate start
+    let reg1 = /[A-Za-zА-Яа-яЁё]/;
+    let reg2 = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+    // const input_name = document.querySelectorAll("input[name=name]");
+    // const input_email = document.querySelectorAll("input[name=email]");
+    const input_btn = document.querySelectorAll("form.feed-form button.button");
+    const btn = document.querySelector("#button");
+    const input = document.querySelector("#input");
+
+    // btn.addEventListener("click", function(e) {
+    //     e.preventDefault();
+    //     if(validate(reg1, input.value)) {
+        //         console.log(input.value);
+    //         console.log("Да");
+    //     } else {
+        //         console.log(input.value);
+    //         console.log("Нет");
+    //     }
+    // });
+    
+    input_btn.forEach(elem => {
+        elem.addEventListener("click", (e) => {
+            const input_name = elem.parentNode.querySelector("input[name=name]");
+            const span = elem.parentNode.querySelector("span");
+            e.preventDefault();
+            if(validate(reg1, input_name.value)) {
+                valid(input_name, span, "")
+                console.log("Да");
+            } else {
+                notValidate(input_name, span, "Проверка не пройдена")
+                console.log("Нет");
+            }
+        });
+    });
+
+    function validate(reg, input) {
+        console.log(reg.test(input));
+        return reg.test(input);
+    }
+
+    function notValidate(input, elem, message) {
+        elem.classList.remove("valid");
+        elem.classList.add("not-valid");
+        elem.innerHTML = message;
+    }
+    function valid(input, elem, message) {
+        elem.classList.remove("not-valid");
+        elem.classList.add("valid");
+        elem.innerHTML = message;
+    }
+
+    // Validate end
 });
 
