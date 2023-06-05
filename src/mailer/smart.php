6430f0239e790db352ece8df4a65f79bc5f6681f
Traniks <andert2017@gmail.com>
2023-06-05 16:38:55 +0300

    check error

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,32 +8,31 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                              // Enable verbose debug output
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  					  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'andert2017@gmail.com';            // Наш логин
-$mail->Password = 'mjvebfkhhnjdpaal';                 // Наш пароль от ящика
+$mail->Username = 'traniks.sites@gmail.com';                 // Наш логин
+$mail->Password = 'owfmtystqfyfkzyf';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('andert2017@gmail.com', 'Pulse');     // От кого письмо 
-$mail->addAddress('lepaga2273@asuflex.com');     	  // Add a recipient
-//$mail->addAddress('ellen@example.com');             // Name is optional
+$mail->setFrom('traniks.sites@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('traniks.sites@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');       // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  // Optional name
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Данные';
-$mail->Body    = '
-		Пользователь оставил данные <br> 
-		Имя: ' . $name . ' <br>
-		Номер телефона: ' . $phone . '<br>
-		E-mail: ' . $email . '';
+$mail->Body    = ' Пользователь оставил данные <br> 
+				   Имя: ' .$name. ' <br>
+				   Номер телефона: ' .$phone. '<br>
+				   E-mail: ' .$email;
 
 if(!$mail->send()) {
     return false;
